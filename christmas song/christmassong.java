93c81ed492fbef2f9ce4e9d3dcb0ab79d009e42b
nchan6 <channorris@ymail.com>
2015-08-31 17:49:40 -0700

    8/31/15

---
+++ christmas song/christmassong.java
@@ -0,0 +1,115 @@
+
+/**
+ * Write a description of class christmassong here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class christmassong
+{
+    public static void main(String[] args){
+        System.out.println("On the first day of Christmas,");
+        tru();
+        dayOne();
+        space();
+        System.out.println("On the second day of Christmas,");
+        tru();
+        dayTwo();
+        space();
+        System.out.println("On the third day of Christmas,");
+        tru();
+        dayThree();
+        space();
+        System.out.println("On the fourth day of Christmas,");
+        tru();
+        dayFour();
+        space();
+        System.out.println("On the fifth day of Christmas,");
+        tru();
+        dayFive();
+        space();
+        System.out.println("On the sixth day of Christmas,");
+        tru();
+        daySix();
+        space();
+        System.out.println("On the seventh day of Christmas,");
+        tru();
+        daySeven();
+        space();
+        System.out.println("On the eighth day of Christmas,");
+        tru();
+        dayEight();
+        space();
+        System.out.println("On the ninth day of Christmas,");
+        tru();
+        dayNine();
+        space();
+        System.out.println("On the tenth day of Christmas,");
+        tru();
+        dayTen();
+        space();
+        System.out.println("On the eleventh day of Christmas,");
+        tru();
+        dayEleven();
+        space();
+        System.out.println("On the twelveth day of Christmas,");
+        tru();
+        dayTwelve();
+    }
+    public static void dayOne(){        
+        System.out.println("a partridge in a pear tree.");
+    }
+    public static void dayTwo(){        
+        System.out.println("two turtle doves, and");
+        dayOne();
+    }
+    public static void dayThree(){
+        System.out.println("three French hens,");
+        dayTwo();
+    }
+    public static void dayFour(){
+        System.out.println("four calling birds,");
+        dayThree();
+    }
+    public static void dayFive(){
+        System.out.println("five golden rings,");
+        dayThree();
+    }
+    public static void daySix(){
+        System.out.println("six geese a-laying,");
+        dayFive();
+    }
+    public static void daySeven(){
+        System.out.println("seven swans a-swimming,");
+        daySix();
+    }
+    public static void dayEight(){
+        System.out.println("eight maids a-milking,");
+        daySeven();
+    }
+    public static void dayNine(){
+        System.out.println("nine ladies dancing,");
+        dayEight();
+    }
+    public static void dayTen(){
+        System.out.println("ten lords a-leaping,");
+        dayNine();
+    }
+    public static void dayEleven(){
+        System.out.println("eleven pipers piping,");
+        dayTen();
+    }
+    public static void dayTwelve(){
+        System.out.println("twelve drummers drummng,");
+        dayEleven();
+    }
+    public static void tru(){
+        System.out.println("my true love sent to me");
+    }
+    public static void space(){
+        System.out.println("");
+    }
+}
+
+        
+        
