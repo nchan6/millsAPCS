c758a2aeb252885136400816c108c1f76b4656a2
Norris <channorris@ymail.com>
2015-10-02 08:53:47 -0700

    flash

---
+++ flash/flash.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class flash here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class flash
+{
+  public static void main (String[] args){
+       System.out.println("                    /");
+       System.out.println("                   //");
+       System.out.println("                  //");
+       System.out.println("      ###########/// #");
+       System.out.println("    ##         ////   #");
+       System.out.println("   #          ////     #");
+       System.out.println("  #          ////       #");
+       System.out.println(" #          /////        #");
+       System.out.println(" #         /////         #");
+       System.out.println("#         //////          #");
+       System.out.println("#        ///////////      #");
+       System.out.println("#       ///////////       #");
+       System.out.println("#      ///////////        #");
+       System.out.println("#          /////          #");
+       System.out.println(" #        /////          #");
+       System.out.println(" #       /////           #");
+       System.out.println("  #      ////           #");
+       System.out.println("   #    ////           #");
+       System.out.println("    #  ////          ##");
+       System.out.println("     # ///           #");
+       System.out.println("       //          #");
+       System.out.println("      //###########");
+       System.out.println("      /");
+    }
+}
