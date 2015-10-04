1ce2b1e8ca38e4caf294544dbc62ad61016f55ea
nchan6 <channorris@ymail.com>
2015-10-04 00:00:43 -0700

    flash

--- the flash/flash.java
+++ the flash/flash.java
@@ -2,182 +2,72 @@
 /**
  * Write a description of class flash here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Norris Chan
+ * 10/3/2015
  */
 public class flash
 {
     public static void main (String[] args){
-        for (int i = 1; i <= 3; i++){
-            for (int j = 1; j <= -i+15; j++){
+        woosh(20,0,0,1,0,0);
+        woosher(9,5,2,0);
+        woosh(8,1,9,2,1,1);
+        woosh(6,1,10,3,2,1);
+        woosh(4,1,11,4,4,1);
+        woosh(3,1,11,4,6,1);
+        woosh(2,1,11,4,8,1);
+        woosh(1,1,11,5,9,1);
+        woosh(1,1,10,5,10,1);
+        woosh(0,1,10,6,11,1);
+        woosh(0,1,9,11,7,1);
+        woosh(0,1,8,11,8,1);
+        woosh(0,1,7,11,9,1);
+        woosh(0,1,11,6,10,1);
+        woosh(1,1,10,5,11,1);
+        woosh(1,1,9,5,11,1);
+        woosh(2,1,8,4,11,1);
+        woosh(3,1,6,4,11,1);
+        woosh(4,1,4,4,11,1);
+        woosh(5,1,3,3,11,1);
+        woosh(7,1,1,2,10,1);
+        woosher(8,0,2,5);
+        woosh(8,0,0,1,0,0);
+    }
+    public static void woosh( int space,int hashtag, int spaces, int slash, int spacess, int hashtags){
+        for (int i = 1; i <= space; i++){
                 System.out.print(" ");
-            }
-            System.out.print("/");
-            for (int k = 2; k <= i; k++){
-                System.out.print("/");
-            }
-            System.out.println();
         }
-        sixSpace();
-        fiveHash();
-        threeSlash();
-        newLine();
-        fourSpace();
-        twoHash();
-        fourSpace();
-        fourSlash();
-        newLine();
-        threeSpace();
-        oneHash();
-        fiveSpace();
-        fourSlash();
-        oneHash();
-        newLine();
-        twoSpace();
-        oneHash();
-        fiveSpace();
-        fourSlash();
-        twoSpace();
-        oneHash();
-        newLine();
-        oneSpace();
-        oneHash();
-        fiveSpace();
-        fiveSlash();
-        threeSpace();
-        oneHash();
-        newLine();
-        oneSpace();
-        oneHash();
-        fourSpace();
-        fiveSlash();
-        fourSpace();
-        oneHash();
-        newLine();
-        oneHash();
-        fourSpace();
-        sixSlash();
-        fiveSpace();
-        oneHash();
-        newLine();
-        oneHash();
-        threeSpace();
-        elevenSlash();
-        oneSpace();
-        oneHash();
-        newLine();
-        oneHash();
-        twoSpace();
-        elevenSlash();
-        twoSpace();
-        oneHash();
-        newLine();
-        oneHash();
-        oneSpace();
-        elevenSlash();
-        threeSpace();
-        oneHash();
-        newLine();
-        oneHash();
-        fiveSpace();
-        sixSlash();
-        fourSpace();
-        oneHash();
-        newLine();
-        oneSpace();
-        oneHash();
-        fourSpace();
-        fiveSlash();
-        fourSpace();
-        oneHash();
-        newLine();
-        oneSpace();
-        oneHash();
-        threeSpace();
-        fiveSlash();
-        fiveSpace();
-        oneHash();
-        newLine();
-        twoSpace();
-        oneHash();
-        twoSpace();
-        fourSlash();
-        fiveSpace();
-        oneHash();
-        newLine();
-        threeSpace();
-        oneHash();
-        fourSlash();
-        fiveSpace();
-        oneHash();
-        newLine();
-        threeSpace();
-        fourSlash();
-        fourSpace();
-        twoHash();
-        newLine();
-        threeSpace();
-        threeSlash();
-        fiveHash();
-        newLine();
-        twoSpace();
-        threeSlash();
-        newLine();
-        twoSpace();
-        twoSlash();
-        newLine();
-        twoSpace();
-        oneSlash();        
+        for (int i = 1; i <= hashtag; i++){
+                System.out.print("#");
+        }
+        for (int i = 1; i <= spaces; i++){
+                System.out.print(" ");
+        }
+        for (int i = 1; i <= slash; i++){
+                System.out.print("/");
+        }
+        for (int i = 1; i <= spacess; i++){
+                System.out.print(" ");
+        }
+        for (int i = 1; i <= hashtags; i++){
+                System.out.print("#");
+        }
+        System.out.println();
     }
-    public static void oneSpace(){
-        System.out.print(" ");
-    }
-    public static void twoSpace(){
-        System.out.print("  ");
-    }
-    public static void threeSpace(){
-        System.out.print("   ");
-    }
-    public static void fourSpace(){
-        System.out.print("    ");
-    }
-    public static void fiveSpace(){
-        System.out.print("     ");
-    }
-    public static void sixSpace(){
-        System.out.print("      ");
-    }
-    public static void oneHash(){
-        System.out.print("#");
-    }
-    public static void twoHash(){
-        System.out.print("##");
-    }
-    public static void fiveHash(){
-        System.out.print("#####");
-    }
-    public static void oneSlash(){
-        System.out.print("/");
-    }
-    public static void twoSlash(){
-        System.out.print("//");
-    }
-    public static void threeSlash(){
-        System.out.print("///");
-    }
-    public static void fourSlash(){
-        System.out.print("////");
-    }
-    public static void fiveSlash(){
-        System.out.print("/////");
-    }
-    public static void sixSlash(){
-        System.out.print("//////");
-    }
-    public static void elevenSlash(){
-        System.out.print("///////////");
-    }
-    public static void newLine(){
+    public static void woosher(  int space,int spaceHashtag, int slash, int spaceHashtags){
+        for (int i = 1; i <= space; i++){
+            System.out.print(" ");
+        }
+        for (int i = 1; i<= spaceHashtag; i++){
+            System.out.print(" #");
+        }
+        for (int i = 1; i <= slash; i++){
+                System.out.print("/");
+        }
+         for (int i = 1; i<= spaceHashtags; i++){
+            System.out.print(" #");
+        }
         System.out.println();
     }
 }
+        
+        
