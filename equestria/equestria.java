f487d52b9acb08da8ef0e14f199853edf147d9c2
Norris <channorris@ymail.com>
2015-10-15 08:53:59 -0700

    equestria

--- equestria/equestria.java
+++ equestria/equestria.java
@@ -8,20 +8,34 @@
 public class equestria
 {
    public static void main(String[] args){
-       System.out.println("Distance from Baltimore to Manehattan = " + distance(32,25,35,9));
-       System.out.println("Distance from Los Pegasus to Neighagra Falls = " + distance(6,18,22,7));
-       System.out.println("Distance from Badlands to Ponyville = " + distance(25,22,16,13));
-       roadTrip(35,9,19,11);
+       System.out.println("Distance from Baltimore to Manehattan = " + distanceCity(32,25,35,9));
+       System.out.println("Distance from Los Pegasus to Neighagra Falls = " + distanceCity(6,18,22,7));
+       System.out.println("Distance from Badlands to Ponyville = " + distanceCity(25,22,16,13));
+       roadTrip(1,2,2,3);
+       System.out.println("angle a is " + angleA);
+       System.out.println("angle b is " + angleB);
+       System.out.println("angle c is " + angleC);
+       
     }
-   public static double distance(int x1, int y1, int x2, int y2){
-       double d=Math.sqrt(x2-x1)*2+(y2-y1)*2;
-       return d;
+   public static double distanceCity(int x1, int y1, int x2, int y2){
+       double distance=Math.sqrt(x2-x1)*2+(y2-y1)*2;
+       return distance;
     }
    public static double roadTrip(int x1, int y1, int x2 , int y2){
-       double d=Math.sqrt(x2-x1)*2+(y2-y1)*2;
-       double c=d*Math.PI;
-       return c;
+       double distance = distanceCity(x1,y1,x2,y2);
+       double circumference=distance*Math.PI;
+       return circumference;
     }
+    //use law of cosine
+   public static double tour(int x1, int y1, int x2, int y2, int x3, int y3){
+       double a = distanceCity(x1,y1,x2,y2);
+       double b = distanceCity(x2,y2,x3,y3);
+       double c = distanceCity(x3,y3,x1,y1);
+       double angleA = Math.asin
+        
+    }
+    
+   
 }
        
        
