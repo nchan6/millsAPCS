69c1c444524fd714d60eaf354fb7c1677901a71a
nchan6 <channorris@ymail.com>
2015-12-08 01:03:59 -0800

    frac calc final

--- FracCalc/FracCalc.java
+++ FracCalc/FracCalc.java
@@ -1,96 +1,145 @@
  
-import java.util.*;
-public class FracCalc {
-    public static void main(String[] args) 
+// Norris Chan
+// 12/1/2015
+ import java.util.*;
+public class FracCalc
+{
+   public static void main(String[] args) 
     {
         Scanner me = new Scanner(System.in);
-        System.out.println("lets do some math! type \"quit to stop\" ");
+        System.out.println("lets do some math! type \"type quit to stop\" ");
         String input = me.nextLine();
         while (!input.equals("quit")){
-            String returningFrac = produceAnswer(input);
-            System.out.println(returningFrac);        
+            String answer = produceAnswer(input);
+            System.out.println(answer);    
+            System.out.println("lets do some math! type \"type quit to stop\" ");
+            input = me.nextLine();
         }
-        
-   }   
-   public static String produceAnswer(String input)
-    { 
-        String firstFrac = "bleh";
-        String operand = "bleh";
-        String secondFrac = "bleh";
-        for (int i=0; i<input.length() ; i++){
-            if (input.charAt(i) == ' '){
-                firstFrac = input.substring(0, i);
-                operand = input.substring(i+1, i+2);
-                secondFrac = input.substring(i+1, input.length());
-            }
+    }
+   public static String produceAnswer(String input){
+        int space = input.indexOf(" ");
+        String firstFrac = input.substring(0, space);
+        String operator = input.substring(space+1,space+2);
+        String secFrac = input.substring(space+3,input.length());
+        String frac1 = parseFrac(firstFrac);
+        String whole1Str = frac1.substring(6,frac1.indexOf(" numer"));
+        int whole1 = Integer.parseInt(whole1Str);
+        String numer1Str = frac1.substring(frac1.indexOf("numerator:")+10,frac1.indexOf(" denom"));
+        int numer1 = Integer.parseInt(numer1Str);
+        String denom1Str = frac1.substring(frac1.indexOf("denominator:")+12,frac1.length());
+        int denom1 = Integer.parseInt(denom1Str);
+        String frac2 = parseFrac(secFrac);
+        String whole2Str = frac2.substring(6,frac2.indexOf(" numer"));
+        int whole2 = Integer.parseInt(whole2Str);
+        String numer2Str = frac2.substring(frac2.indexOf("numerator:")+10,frac2.indexOf(" denom"));
+        int numer2 = Integer.parseInt(numer2Str);
+        String denom2Str = frac2.substring(frac2.indexOf("denominator:")+12,frac2.length());
+        int denom2 = Integer.parseInt(denom2Str);
+        int impropNumer1 = Math.abs(denom1*whole1)+numer1;
+        int impropNumer2 = Math.abs(denom2*whole2)+numer2;        
+        String ans = "bleh";
+        if (operator.equals("+")){
+            ans = add(impropNumer1,denom1,impropNumer2,denom2);
         }
-        String first = parseFrac(firstFrac);
-        String second = parseFrac(secondFrac);
-        String wholeFirstStr = first.substring(6,first.indexOf(" numerator:"));
-        int wholeFirst = Integer.parseInt(wholeFirstStr);
-        String numerFirstStr = first.substring(first.indexOf("numerator:")+10,first.indexOf(" denominator:"));
-        int numerFirst = Integer.parseInt(numerFirstStr);
-        String denomFirstStr = first.substring(first.indexOf("denominator:")+12,first.length()-3);
-        int denomFirst = Integer.parseInt(denomFirstStr);
-        String wholeSecStr = second.substring(6,second.indexOf(" numerator:"));
-        int wholeSec = Integer.parseInt(wholeSecStr);
-        String numerSecStr = second.substring(second.indexOf("numerator:")+10,second.indexOf(" denominator:"));
-        int numerSec = Integer.parseInt(numerSecStr);
-        String denomSecStr = second.substring(second.indexOf("denominator:")+12,second.length()-1);
-        int denomSec = Integer.parseInt(denomSecStr);
-        int impropFracOneNum = (denomFirst*wholeFirst)+numerFirst;
-        int impropFracTwoNum = (denomSec*wholeSec)+numerSec;
-        int finalWhole = 0;
-        int finalNumerator = 0;
-        int finalDenominator = 0;
-        if (operand == "+"){
-            finalWhole = wholeFirst + wholeSec;
-            finalNumerator = (numerFirst*denomSec)+(numerSec*denomFirst);
-            finalDenominator = denomFirst*denomSec;
-        }else if (operand == "-"){
-            finalWhole = wholeFirst - wholeSec;
-            finalNumerator = (numerFirst*denomSec)-(numerSec*denomFirst);
-            finalDenominator = denomFirst*denomSec;
-        }else if (operand == "/"){
-            finalWhole = 0;
-            finalNumerator = impropFracOneNum*denomSec;
-            finalDenominator = impropFracTwoNum*denomFirst;
-        }else if (operand == "*"){
-            finalWhole = 0;
-            finalNumerator = impropFracOneNum*impropFracTwoNum;
-            finalDenominator = denomFirst*denomSec;
+        if (operator.equals("-")){
+            ans = sub(impropNumer1,denom1,impropNumer2,denom2);
         }
-        String answer = finalWhole+"_"+finalNumerator+"/"+finalDenominator;
-        return answer;
-    }        
-   public static String parseFrac(String fract)
-   {
-        String whole = "bleh";
-        String numerator = "bleh";
-        String denominator = "bleh";
-        boolean mixedNum = false;
-        boolean frac = false;
-        for (int i=0; i<fract.length(); i++){
-            if (fract.charAt(i) == '_'){
-                mixedNum = true;                
-            }
-            if (fract.charAt(i) == '/'){
-                frac = true;
-            }
+        if (operator.equals("/")){
+            ans = div(impropNumer1,denom1,impropNumer2,denom2);
         }
-        if (mixedNum){
-            whole = fract.substring(0, fract.indexOf('_'));
-            numerator = fract.substring(fract.indexOf('_')+1, fract.indexOf('/'));
-            denominator = fract.substring(fract.indexOf('/')+1, fract.length());                     
-        } else if (frac){
-            whole = "0";
-            numerator = fract.substring(0, fract.indexOf('/'));
-            denominator = fract.substring(fract.indexOf('/')+1, fract.length());
-        } else {
-            whole = fract;
-            numerator = "0";
-            denominator = "1";
+        if (operator.equals("*")){
+            ans = mult(impropNumer1,denom1,impropNumer2,denom2);
         }
-        return "whole:"+whole+" numerator:"+numerator+" denominator:"+denominator+" "; 
+        return ans;
+    }   
+   public static String parseFrac(String fract){
+       String whole = "bleh";
+       String numerator = "bleh";
+       String denominator = "bleh";
+       boolean mixedNum = false;
+       boolean frac = false;
+       for (int i=0; i<fract.length(); i++){
+           if (fract.charAt(i) == '_'){
+               mixedNum = true;                
+          }
+          if (fract.charAt(i) == '/'){
+               frac = true;
+           }
+       }
+       if (mixedNum){
+           whole = fract.substring(0, fract.indexOf('_'));
+           numerator = fract.substring(fract.indexOf('_')+1, fract.indexOf('/'));
+           denominator = fract.substring(fract.indexOf('/')+1, fract.length());                     
+       } else if (frac){
+           whole = "0";
+           numerator = fract.substring(0, fract.indexOf('/'));
+           denominator = fract.substring(fract.indexOf('/')+1, fract.length());
+       } else {
+           whole = fract;
+           numerator = "0";
+           denominator = "1";
+       }
+       return "whole:"+whole+" numerator:"+numerator+" denominator:"+denominator; 
    }
-}
+   public static String add(int numer1,int denom1,int numer2, int denom2){
+       int numer = (numer1*denom2)+(numer2*denom1);
+       int denom = denom1*denom2;
+       String ans = simp(numer,denom);
+       return ans;
+   }
+    public static String sub(int numer1,int denom1,int numer2, int denom2){
+       int numer = (numer1*denom2)-(numer2*denom1);
+       int denom = denom1*denom2;
+       String ans = simp(numer,denom);
+       return ans;
+   }
+    public static String div(int numer1,int denom1,int numer2, int denom2){
+       int numer = numer1/denom2;
+       int denom = numer2*denom1;
+       String ans = simp(numer,denom);
+       return ans;
+   }
+   public static String mult(int numer1,int denom1,int numer2, int denom2){
+       int numer = numer1*numer2;
+       int denom = denom1*denom2;
+       String ans = simp(numer,denom);
+       return ans;
+   }
+   public static String simp(int numer,int denom){
+       int whole = 0;
+       String ans = "bleh";
+       if (numer%denom == 0){
+           whole = numer/denom;
+           ans = whole+"";
+       }else if (numer > denom){
+           whole = numer/denom;
+           numer = numer%denom;
+           int comFact = gcf(numer,denom);
+           numer = numer/comFact;
+           denom = denom/comFact;
+           ans = whole+"_"+numer+"/"+denom;
+       } else if (numer < denom){
+           int comFact = gcf(numer,denom);
+           numer = numer/comFact;
+           denom = denom/comFact;
+           ans = numer+"/"+denom;
+       }
+       return ans;
+   }
+   public static int gcf(int numer,int denom){
+        while(numer!=0 && denom!=0) {
+            int c =  denom;
+            denom = numer%denom;
+            numer = c;
+        }
+        return Math.abs(numer+denom);
+    }
+
+   }
+
+               
+            
+       
+   
+
+    
