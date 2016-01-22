de0d262e605e2bc9622e0f1d38d404d626b65f93
Norris <channorris@ymail.com>
2016-01-22 08:53:35 -0800

    golf

---
+++ golf program/GolfProblem.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class GolfProblem here.
+ * 
+ * Norris Chan 
+ * 1/22/16
+ */
+import.java.util.scanner;
+public class GolfProblem
+{
+   public static void main(String[] args){
+       Scanner input = new Scanner(System.in);
+       System.out.println("Enter your score");
+       int givenPar = input.nextInt();
+       int playerScore = input.nextInt();
+       
+    }
+}
