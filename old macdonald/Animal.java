3d61e8a695081200b9f391c157c1732ad8fac3c0
Norris <channorris@ymail.com>
2016-03-03 08:46:17 -0800

    farm

---
+++ old macdonald/Animal.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Animal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Animal
+{
+    public String getSound(){
+        return mySound;
+    }
+    public String getType(){
+        return myType;
+    }
+}
---
+++ old macdonald/Cow.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Cow here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cow implements Animal
+{
+    private String myType;
+    private String mySound;
+    cow(String type, String sound){
+        myType = type;
+        mySound = sound;
+    }
+}
